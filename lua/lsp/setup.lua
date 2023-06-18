c15cbdd041a289d2517e471b280495384d212e0e
wanzzhehe <wanzzmicro@outlook.com>
2023-06-18 20:59:39 +0800

    remove lua lsp

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -10,7 +10,7 @@ require('lsp.nvim-cmp')
 -- set lsp
 require('lsp.lsp-configs.clangd')
 require('lsp.lsp-configs.pyright')
-require('lsp.lsp-configs.lua_ls')
+--require('lsp.lsp-configs.lua_ls')
 require('lsp.lsp-configs.vimls')
 require('lsp.lsp-configs.vhdl_ls')
 require('lsp.lsp-configs.hdl_checker')
